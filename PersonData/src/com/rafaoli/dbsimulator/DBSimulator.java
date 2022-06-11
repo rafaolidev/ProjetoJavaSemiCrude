594396ff3054fbb20669c3d012751c87bf34df51
Bruno Silva <bos.bruno.oliveira55@gmail.com>
2022-06-10 21:39:07 -0300

    Teste adicionando pessoas

--- PersonData/src/com/rafaoli/dbsimulator/DBSimulator.java
+++ PersonData/src/com/rafaoli/dbsimulator/DBSimulator.java
@@ -1,10 +1,11 @@
 package com.rafaoli.dbsimulator;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.rafaoli.domain.Pessoa;
 
 public class DBSimulator {
 	
-	public static List<Pessoa> LISTA_DE_PESSOA;	
+	public static List<Pessoa> LISTA_DE_PESSOA = new ArrayList<Pessoa>();	
 }
